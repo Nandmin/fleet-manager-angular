e4a7c43bff139905139e3c08dd5cea4cf55fee31
Nandmin <andras78.nemeth@gmail.com>
2022-02-03 11:05:06 +0100

    GoogleChart working with db.json data

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -11,6 +11,8 @@ import { GoogleChartInterface, GoogleChartType, Ng2GoogleChartsModule } from 'ng
 export class HomeComponent implements OnInit {
 
   vehicleChart!: GoogleChartInterface;
+  driversChart!: GoogleChartInterface;
+  daysChart!: GoogleChartInterface;
   
   pieChartOptions: any = {
     is3D: true,
@@ -34,24 +36,71 @@ export class HomeComponent implements OnInit {
   // ha ok, akkor első sor, ha nem, akkor 2. sor
   .then(
     data => { 
-      this.processData(data)
+      this.initCharts(data)
     },
     err => console.error(err)
   )
   }
 
-  processData(data: any): void {
-    this.vehicleChart = {
-      chartType: 'PieChart',
+  initCharts(data: any): void {
+    let ByVehicle = this.processByType(data, (row: any) => row.vehicle.lp, (row: any) => row.amount);
+    let ByDriver = this.processByType(data, (row: any) => row.driver.name, (row: any) => row.amount);
+    let ByDays = this.processByType(data, (row: any) => row.date, (row: any) => row.amount);
+
+    console.log(ByVehicle);
+    
+    this.vehicleChart = this.collectChartData(
+      [['Vehicle', 'Consumption']].concat(ByVehicle), 'PieChart', this.pieChartOptions);
+    
+    this.driversChart = this.collectChartData(
+      [['Vehicle', 'Consumption']].concat(ByDriver), 'PieChart', this.pieChartOptions);
+
+    this.daysChart = this.collectChartData(
+      [['Vehicle', 'Consumption']].concat(ByDays), 'ColumnChart', this.columnChartOptions);
+    
+    // {
+    //   chartType: 'PieChart',
+    //   //chartType: GoogleChartType.PieChart,
+    //   dataTable: [
+    //     ['Vehicle', 'Cosumption'],
+    //     ['GGP-432', 40],
+    //     ['HHH-432', 100],
+    //     ['UUU-432', 10]
+    //   ],
+    //   //firstRowIsData: true,
+    //   options: this.pieChartOptions
+    // }
+  }
+
+
+  collectChartData(data: any[], type: string, options: any): any {
+    return {
+      chartType: type,
       //chartType: GoogleChartType.PieChart,
-      dataTable: [
-        ['Vehicle', 'Cosumption'],
-        ['GGP-432', 40],
-        ['HHH-432', 100],
-        ['UUU-432', 10]
-      ],
+      dataTable: data,
       //firstRowIsData: true,
-      options: this.pieChartOptions
+      options: options
     }
   }
+
+  // típus alapján végzi el az adatfeldolgozást
+  processByType(data: any[], getkey: Function, getValue: any): any[] {
+    let processed: any = {};
+    let table: any[] = [];
+
+    for(let row of data){
+      let key = getkey(row);
+
+      if(!processed[getkey(row)]){
+        processed[getkey(row)] = 0;
+      }
+      processed[key] += parseInt(getValue(row));
+    }
+    for(let k in processed){
+      table.push([k, processed[k]]);
+    }
+
+    return table;
+  }
 }
+
